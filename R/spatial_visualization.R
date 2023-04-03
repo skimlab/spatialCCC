481818ce342c873fecbe9992825fbd6d477d480e
Seungchan Kim <dolchan@gmail.com>
2023-04-03 08:36:34 -0500

    plot_spatial_ccc_graph() added

---
+++ R/spatial_visualization.R
@@ -0,0 +1,432 @@
+#' Plot spatial CCC graph
+#'
+#' Visualize spatial CCC graph in various formats,
+#'   w/ or w/o tissue image
+#'
+#'   * CCC graph with tissue image. In this format,
+#'   the spots are placed on their original locations, keeping
+#'   spatial relations.
+#'
+#'   * CCC graph without tissue image. In this format,
+#'    the spots are placed using user-specified graph layout
+#'    algorithm.  Default is ("kk")[igraph::layout_with_kk()] algorithm.
+#'    See [ggraph::create_layout()] for available algorithms.
+#'
+#' @param ccc_graph spatial CCC graph for a LR pair
+#' @param graph_layout string for graph layout algorithm. Default is "auto";
+#'   see Details for more information
+#' @param cells_of_interest array of cell ids to highlight.
+#' @param edges_expanded_to_group if TRUE and cells_of_interest is given,
+#'   the nodes and the edges are expanded to include all the nodes and edges that
+#'   belong to the groups that cells_of_interest belong to.
+#' @param edge_color,node_color column name to be used
+#'   for edge and node colors, respectively
+#' @param edge_range,node_range bound given in c(min, max) format.
+#'   if specified, the ranges are set using these values, instead of the ones
+#'   derived from data.  Useful when multiple plots are combined
+#'   later for comparison.
+#' @param edge_width,node_size specify edge width and node size.  Currently,
+#'   default values do not work well.  Might need some manual adjustments.
+#' @param edge_alpha,node_alpha specify transparency of edges and nodes
+#' @param clip if TRUE, tissue_img will be cliped to show only active cell-cell
+#'   communications
+#' @param tissue_img Raster image, like imgRaster(spe)
+#' @param which_on_top either "edge" or "node"
+#' @param show_arrow if TRUE, arrow is added to edge.  Default is FALSE.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' plot_spatial_ccc_graph(ccc_graph = ccc_graph_list[["App_Dcc"]],
+#'                        tissue_img = imgRaster(spe),
+#'                        node_color = "group_diameter",
+#'                        node_size = 1,
+#'                        edge_color = "group_diameter",
+#'                        which_on_top = "edge")
+#'
+#' plot_spatial_ccc_graph(ccc_graph = ccc_graph_list[["App_Dcc"]],
+#'                        node_color = "group_diameter",
+#'                        node_size = 1,
+#'                        edge_color = "group_diameter",
+#'                        which_on_top = "edge")
+#'
+#' plot_spatial_ccc_graph(ccc_graph = ccc_graph_list[["App_Dcc"]],
+#'                        graph_layout = "stress",
+#'                        node_color = "group_diameter",
+#'                        node_size = 1,
+#'                        edge_color = "group_diameter",
+#'                        which_on_top = "edge")
+#' }
+#'
+plot_spatial_ccc_graph <-
+  function(ccc_graph,
+           graph_layout = "auto",
+           cells_of_interest = NULL,
+           edges_expanded_to_group = TRUE,
+           edge_color = "group_n_edges",
+           node_color = "group_n_nodes",
+           edge_range = NULL,
+           node_range = NULL,
+           edge_width = 0.5,
+           edge_alpha = 1,
+           node_size = 0.5,
+           node_alpha = 1,
+           clip = FALSE,
+           tissue_img = NULL,
+           which_on_top = "edge",
+           show_arrow = FALSE) {
+
+    cells_of_interest_given <- !is.null(cells_of_interest)
+
+    if (cells_of_interest_given) {
+      ccc_graph %<>%
+        tidygraph::activate(nodes) %>%
+        tidygraph::mutate(InCOI = name %in% cells_of_interest) %>%
+        tidygraph::mutate(InFocus = InCOI) %>%
+        tidygraph::activate(edges) %>%
+        tidygraph::mutate(InCOI = src %in% cells_of_interest |
+                 dst %in% cells_of_interest) %>%
+        tidygraph::mutate(InFocus = InCOI)
+
+      if (edges_expanded_to_group) {
+        groups_of_interest <-
+          ccc_graph %>%
+          tidygraph::activate(nodes) %>%
+          tidygraph::filter(InFocus) %>%
+          tidygraph::pull(group) %>%
+          unique()
+
+        cells_in_GOI <-
+          ccc_graph %>%
+          tidygraph::activate(nodes) %>%
+          tidygraph::filter(group %in% groups_of_interest) %>%
+          tidygraph::pull(name) %>%
+          unique()
+
+        ccc_graph %<>%
+          tidygraph::activate(edges) %>%
+          tidygraph::mutate(InGOI = src %in% cells_in_GOI |
+                            dst %in% cells_in_GOI) %>%
+          tidygraph::mutate(InFocus = InGOI)
+      }
+    } else {
+      ccc_graph %<>%
+        tidygraph::activate(nodes) %>%
+        tidygraph::mutate(InFocus = TRUE) %>%
+        tidygraph::activate(edges) %>%
+        tidygraph::mutate(InFocus = TRUE)
+    }
+
+    # ccc_graph %<>%
+    #   activate(nodes) %>%
+    #   filter(InFocus)
+
+    ccc_graph_nodes <-
+      ccc_graph %>%
+      tidygraph::activate(nodes) %>%
+      tibble::as_tibble()
+
+    ccc_edge_palette <-
+      # RdYlBu is an alternative to Spectral
+      colorRampPalette(rev(brewer.pal(n = 7, name = "Spectral")))
+
+    if (is.null(edge_range)) {
+      edge_color_values <-
+        ccc_graph %>%
+        tidygraph::activate(edges) %>%
+        tidygraph::pull(get(edge_color))
+
+      min_edge_color <- min(edge_color_values)
+      max_edge_color <- max(edge_color_values)
+    }
+
+    if (max_edge_color * min_edge_color < 0) {
+      mm <- max(abs(max_edge_color), abs(min_edge_color))
+      max_edge_color <- mm
+      min_edge_color <- -mm
+    }
+
+    ccc_node_palette <-
+      colorRampPalette(rev(RColorBrewer::brewer.pal(7, "Spectral")))
+
+    # check if node_color is discrete
+    node_color_values <-
+      ccc_graph %>%
+      tidygraph::activate(nodes) %>%
+      tidygraph::pull(get(node_color))
+
+    node_color_is_discrete <-
+      all(node_color_values == as.integer(node_color_values))
+
+    if (node_color_is_discrete) {
+      node_color_is_discrete <-
+        length(unique(node_color_values)) <= 15
+    }
+
+    if (is.null(node_range)) {
+      max_node_color <- max(node_color_values)
+      min_node_color <- min(node_color_values)
+    }
+
+    if (max_node_color * min_node_color < 0) {
+      mm <- max(abs(max_node_color), abs(min_node_color))
+      max_node_color <- mm
+      min_node_color <- -mm
+    }
+
+
+    node_size_original <- node_size
+    node_alpha_original <- node_alpha
+
+    edge_width_original <- edge_width
+    edge_alpha_original <- edge_alpha
+
+    if (which_on_top == "edge") {
+      node_alpha = 0.5 * node_alpha_original
+      #node_size = 0.5 * node_size
+    } else {
+      edge_alpha = 0.5 * edge_alpha_original
+      #edge_width = 0.5 * edge_width
+    }
+
+    edge_alpha_range <- c(0.1, edge_alpha)
+    node_alpha_range <- c(0.1, node_alpha)
+
+
+    if (!is.null(tissue_img)) {
+      #
+      # add tissue image
+      #
+      # tissue_img <- imgRaster(spe)
+      img_width <- ncol(tissue_img)
+      img_height <- nrow(tissue_img)
+
+      x_min <- 0
+      x_max <- img_width
+      y_min <- 0
+      y_max <- img_height
+
+      if (clip) {
+        x_min <- max(x_min, floor(min(ccc_graph_nodes$spot_x)) - 1)
+        x_max <- min(x_max, ceiling(max(ccc_graph_nodes$spot_x)) + 1)
+        y_min <- max(y_min, floor(min(ccc_graph_nodes$spot_y)) - 1)
+        y_max <- min(y_max, ceiling(max(ccc_graph_nodes$spot_y)) + 1)
+
+        margin <- round(0.05 * min(x_max - x_min, y_max - y_min))
+        x_min <- max(x_min - margin, 0)
+        x_max <- min(x_max + margin, img_width)
+        y_min <- max(y_min - margin, 0)
+        y_max <- min(y_max + margin, img_height)
+
+      }
+
+      tissue_img <- tissue_img[y_min:y_max, x_min:x_max]
+
+      spatial_image_grob <- grid::rasterGrob(tissue_img)
+
+      #
+      # create ggraph object
+      #
+      graph_layout <- "manual"
+      ggraph_ccc <-
+        create_layout(
+          graph = ccc_graph,
+          layout = "manual",
+          ## ignore graph_layout option
+          x = spot_x,
+          y = spot_y
+        ) %>%
+        ggraph()
+
+      # add tissue image
+      ggraph_ccc <-
+        ggraph_ccc +
+        geom_spatial(
+          data = tibble::tibble_row(sample = "sample",
+                                    grob = spatial_image_grob),
+          aes(grob = grob),
+          x = 0.5,
+          y = 0.5
+        )
+    } else {
+      #
+      # create ggraph object
+      #
+      if (graph_layout == "spatial") {
+        ggraph_ccc <-
+          create_layout(
+            graph = ccc_graph,
+            layout = "manual",
+            ## ignore graph_layout option
+            x = spot_x,
+            y = spot_y
+          ) %>%
+          ggraph()
+      } else {
+        # some layout alg. does not work
+        # so, default back to stress
+        # otherwise, left to the user
+        if (graph_layout == "auto")
+          graph_layout <- "kk"
+
+        ggraph_ccc <-
+          ggraph(graph = ccc_graph,
+                 layout = graph_layout)
+      }
+    }
+
+    add_ggraph_nodes <- function(gg) {
+      if (node_color_is_discrete) {
+        unique_node_color_values <- sort(unique(node_color_values))
+        node_color_discrete <-
+          ccc_node_palette(n = length(unique_node_color_values))
+        names(node_color_discrete) <- unique_node_color_values
+
+        gg +
+          geom_node_point(
+            aes(# color = factor(get(node_color)),
+              fill = factor(get(node_color)),
+              alpha = as.numeric(InFocus)),
+            pch = 21,
+            color = "black",
+            size = node_size
+          ) +
+          scale_color_manual(name = "node", values = node_color_discrete) +
+          scale_fill_manual(name = "node", values = node_color_discrete) +
+          guides(color = guide_legend(override.aes = list(size = node_size_original * 2))) +
+          scale_alpha(guide = FALSE) +
+          scale_size(guide = FALSE)
+      } else {
+        gg +
+          geom_node_point(
+            aes(
+              # color = get(node_color),
+              fill = get(node_color),
+              alpha = as.numeric(InFocus)
+            ),
+            pch = 21,
+            color = "black",
+            size = node_size
+          ) +
+
+          # for all nodes
+          # alpha = node_alpha,
+          # size = node_size
+          scale_color_gradientn(
+            name = "node",
+            colours = ccc_node_palette(n = 15),
+            limits = c(min_node_color, max_node_color)
+          ) +
+          scale_fill_gradientn(
+            name = "node",
+            colours = ccc_node_palette(n = 15),
+            limits = c(min_node_color, max_node_color)
+          ) +
+          guides(color = guide_legend(override.aes = list(size = node_size_original * 2))) +
+          scale_alpha(range = node_alpha_range, guide = FALSE) +
+          scale_size(guide = FALSE)
+      }
+    }
+
+  add_ggraph_edges <- function(gg) {
+    gg +
+      geom_edge_link(
+        aes(color = get(edge_color),
+            alpha = as.numeric(InFocus)),
+
+        # for all edges
+        # alpha = edge_alpha,
+        # width = edge_width,
+
+        # add arrow
+        arrow = arrow(
+          angle = 15,
+          type = "closed",
+          length = unit(ifelse(show_arrow,
+                               0.01 * edge_width,
+                               0), "npc")
+        )
+      ) +
+      scale_edge_color_gradientn(
+        name = "edge",
+        colours = ccc_edge_palette(n = 15),
+        limits = c(min_edge_color, max_edge_color)
+      ) +
+      scale_edge_alpha(range = edge_alpha_range, guide = FALSE)
+  }
+
+  if (which_on_top == "edge") {
+    ggraph_ccc %<>%
+      add_ggraph_nodes() %>%
+      add_ggraph_edges
+    # guides(size = FALSE, alpha = FALSE)
+  } else {
+    ggraph_ccc %<>%
+      add_ggraph_edges() %>%
+      add_ggraph_nodes()
+  }
+
+  if (!is.null(tissue_img)) {
+    ggraph_ccc <-
+      ggraph_ccc +
+      # set boundary
+      xlim(x_min, x_max) +
+      ylim(y_max, y_min) +
+      coord_fixed(expand = FALSE) +
+      # remove background
+      ggplot2::theme_void()
+  } else {
+    ggraph_ccc <-
+      ggraph_ccc +
+      # theme_graph()
+      # remove background
+      theme_graph()
+  }
+
+  ggraph_ccc
+}
+
+#
+# Internal functions =====
+#
+
+#' Geom Spatial to visualize tissue image
+#'
+#' internal function
+geom_spatial <- function(mapping = NULL,
+                         data = NULL,
+                         stat = "identity",
+                         position = "identity",
+                         na.rm = FALSE,
+                         show.legend = NA,
+                         inherit.aes = FALSE,
+                         ...) {
+  GeomCustom <- ggproto(
+    "GeomCustom",
+    Geom,
+    setup_data = function(self, data, params) {
+      data <- ggproto_parent(Geom, self)$setup_data(data, params)
+      data
+    },
+
+    draw_group = function(data, panel_scales, coord) {
+      vp <- grid::viewport(x = data$x, y = data$y)
+      g <- grid::editGrob(data$grob[[1]], vp = vp)
+      ggplot2:::ggname("geom_spatial", g)
+    },
+
+    required_aes = c("grob", "x", "y")
+  )
+
+  layer(
+    geom = GeomCustom,
+    mapping = mapping,
+    data = data,
+    stat = stat,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
--- README.md
+++ README.md
@@ -53,9 +53,32 @@ spe <- SpatialExperiment::read10xVisium(samples = data_dir, type = "HDF5", data 
 ## Ligand-Receptor Database: built-in
 
 ``` r
-LRdb_mouse <- 
-  get_LRdb("mouse") %>%
-  dplyr::slice_sample(n = 50)
+LRdb_m <- 
+  get_LRdb("mouse") 
+
+LR_pair_list <- c(
+  "Apoe_Sdc1",
+  "App_Dcc",
+  "Cxcl12_Ackr3",
+  "Jam3_Itgam",
+  "Cx3cl1_Itga4")
+
+LRdb_m <-
+  dplyr::filter(LRdb_m, LR %in% LR_pair_list)
+
+LRdb_m %>% 
+  arrange(ligand_gene_symbol, receptor_gene_symbol)
+#> # A tibble: 5 × 10
+#>   LR     ligand_gene_symbol receptor_gene_symbol ligand_gene_id receptor_gene_id
+#>   <chr>  <chr>              <chr>                         <dbl>            <dbl>
+#> 1 Apoe_… Apoe               Sdc1                          11816            20969
+#> 2 App_D… App                Dcc                           11820            13176
+#> 3 Cx3cl… Cx3cl1             Itga4                         20312            16401
+#> 4 Cxcl1… Cxcl12             Ackr3                         20315            12778
+#> 5 Jam3_… Jam3               Itgam                         83964            16409
+#> # ℹ 5 more variables: ligand_ensembl_protein_id <chr>,
+#> #   receptor_ensembl_protein_id <chr>, ligand_ensembl_gene_id <chr>,
+#> #   receptor_ensembl_gene_id <chr>, evidence <chr>
 ```
 
 ## Log-Normalize
@@ -74,9 +97,9 @@ spe <- logNormCounts(spe)
 tictoc::tic()
 ccc_tbl <- compute_spatial_ccc(spe = spe, 
                                assay_name = "logcounts",
-                               LRdb = LRdb_mouse)
+                               LRdb = LRdb_m)
 tictoc::toc()
-#> 16.888 sec elapsed
+#> 18.077 sec elapsed
 
 # future::plan(future::sequential)
 ```
@@ -84,20 +107,20 @@ tictoc::toc()
 ``` r
 ccc_tbl %>% 
   dplyr::arrange(desc(LRscore))
-#> # A tibble: 43,276 × 10
+#> # A tibble: 8,083 × 10
 #>    src          dst       d norm.d LR    ligand receptor LRscore weight WLRscore
 #>    <chr>        <chr> <dbl>  <dbl> <chr> <chr>  <chr>      <dbl>  <dbl>    <dbl>
-#>  1 ACCGCGGTGGA… TCTT…  138.   1.00 Apoe… Apoe   Lrp1       0.880  0.992    0.873
-#>  2 GCAACAGCAGT… TCTT…  138.   1.01 Apoe… Apoe   Lrp1       0.879  0.980    0.861
-#>  3 TGCTGGTTGGA… TTGT…  138.   1.01 Apoe… Apoe   Lrp1       0.878  0.980    0.860
-#>  4 TCGTCCGCTGG… TTGT…  137    1    Apoe… Apoe   Lrp1       0.877  1        0.877
-#>  5 CCATGCTCTGC… TTGT…  138.   1.01 Apoe… Apoe   Lrp1       0.876  0.980    0.858
-#>  6 ATCGCCAGTCA… TCTT…  138    1.01 Apoe… Apoe   Lrp1       0.875  0.986    0.863
-#>  7 TTCGCTATCTG… GGCT…  138.   1.01 Apoe… Apoe   Lrp1       0.875  0.980    0.857
-#>  8 TTCTAGGCCAA… TTGT…  138    1.01 Apoe… Apoe   Lrp1       0.873  0.986    0.861
-#>  9 TAACAAAGGGA… GCTT…  138.   1.01 Apoe… Apoe   Lrp1       0.873  0.987    0.861
-#> 10 TTCTTGTAACC… GCTT…  138.   1.01 Apoe… Apoe   Lrp1       0.873  0.987    0.861
-#> # ℹ 43,266 more rows
+#>  1 GCTATCGCGGC… CCCG…  138.   1.01 App_… App    Dcc        0.821  0.980    0.804
+#>  2 CTGGCGGGAAT… CCCG…  138.   1.01 App_… App    Dcc        0.819  0.980    0.803
+#>  3 CTGGAAATGGA… TAGA…  138    1.01 App_… App    Dcc        0.818  0.986    0.806
+#>  4 GAGACTGATGG… TAGA…  138.   1.01 App_… App    Dcc        0.816  0.980    0.800
+#>  5 TAGCTAAGTCC… TAGA…  137    1    App_… App    Dcc        0.816  1        0.816
+#>  6 TGATCGGTTTG… TAGA…  138.   1.01 App_… App    Dcc        0.815  0.980    0.799
+#>  7 TTCGGCTAGAG… CCCG…  138.   1.01 App_… App    Dcc        0.815  0.980    0.799
+#>  8 CATCCTCTCAA… CATG…  138    1.01 App_… App    Dcc        0.814  0.986    0.802
+#>  9 GACACGAGTTA… CCCG…  138    1.01 App_… App    Dcc        0.813  0.986    0.801
+#> 10 GATCATTCCAA… TAGA…  138.   1.01 App_… App    Dcc        0.812  0.980    0.796
+#> # ℹ 8,073 more rows
 ```
 
 ``` r
@@ -109,7 +132,7 @@ ccc_graph_list <-
   to_spatial_ccc_graph_list(ccc_tbl, sp_col_data, workers = 4)
 
 tictoc::toc()
-#> to_spatical_ccc_graph ...: 13.731 sec elapsed
+#> to_spatical_ccc_graph ...: 6.397 sec elapsed
 ```
 
 ``` r
@@ -119,30 +142,19 @@ ccc_graph_metrics_summary_df <-
   summarize_ccc_graph_metrics(ccc_graph_list)
 
 tictoc::toc()
-#> 0.062 sec elapsed
+#> 0.023 sec elapsed
 ```
 
 ``` r
-ccc_graph_metrics_summary_df 
-#> # A tibble: 16 × 12
-#>    LR        graph_n_nodes graph_n_edges graph_component_count graph_motif_count
-#>    <chr>             <int>         <dbl>                 <dbl>             <int>
-#>  1 Trf_Tfr2           1227          1520                    83              3481
-#>  2 Jag1_Not…           346           263                   108               210
-#>  3 Igf2_Igf…          1074          1265                   134              1941
-#>  4 Col4a1_C…           583           516                   138               570
-#>  5 Nlgn1_Nr…          2580          9213                     3             20396
-#>  6 Icam2_It…           151            99                    56                45
-#>  7 Inhba_Ac…           314           349                    58               521
-#>  8 Apoe_Lrp1          2701         14893                     3             27822
-#>  9 Mdk_Itgb1          2124          5200                    17             11114
-#> 10 Fgf13_Fg…          2363          5649                    16             13251
-#> 11 Adam12_I…            83            52                    33                21
-#> 12 Wnt5b_Lr…           345           278                    91               295
-#> 13 Fgf2_Not…           424           340                   108               328
-#> 14 Vip_Vipr1           609           684                    77              1092
-#> 15 Sema4d_M…           647           787                    65              1393
-#> 16 Efnb1_Ep…          1457          2168                    75              4431
+ccc_graph_metrics_summary_df %>%
+  arrange(graph_component_count)
+#> # A tibble: 4 × 12
+#>   LR         graph_n_nodes graph_n_edges graph_component_count graph_motif_count
+#>   <chr>              <int>         <dbl>                 <dbl>             <int>
+#> 1 App_Dcc             1989          3267                    42              8152
+#> 2 Cx3cl1_It…          1400          1994                    65              4790
+#> 3 Cxcl12_Ac…          1171          1418                   112              2436
+#> 4 Jam3_Itgam          1191          1404                   118              2317
 #> # ℹ 7 more variables: graph_diameter <dbl>, graph_un_diameter <dbl>,
 #> #   graph_mean_dist <dbl>, graph_circuit_rank <dbl>, graph_reciprocity <dbl>,
 #> #   graph_clique_num <int>, graph_clique_count <int>
@@ -155,26 +167,73 @@ ccc_graph_group_metrics_summary_df <-
   summarize_ccc_graph_metrics(ccc_graph_list, level = "group")
 
 tictoc::toc()
-#> 0.053 sec elapsed
+#> 0.014 sec elapsed
 ```
 
 ``` r
 ccc_graph_group_metrics_summary_df 
-#> # A tibble: 1,065 × 12
-#>    LR       group group_n_nodes group_n_edges group_adhesion group_motif_count
-#>    <chr>    <int>         <int>         <dbl>          <dbl>             <int>
-#>  1 Trf_Tfr2     5            45            58              0               109
-#>  2 Trf_Tfr2    31            13            12              0                31
-#>  3 Trf_Tfr2    45             7             6              0                15
-#>  4 Trf_Tfr2    79             4             3              0                 3
-#>  5 Trf_Tfr2    23            15            18              0                45
-#>  6 Trf_Tfr2    46             7             6              0                15
-#>  7 Trf_Tfr2    16            22            42              0                95
-#>  8 Trf_Tfr2    18            20            29              0                64
-#>  9 Trf_Tfr2     4            50            59              0               154
-#> 10 Trf_Tfr2     3            57            79              0               173
-#> # ℹ 1,055 more rows
+#> # A tibble: 337 × 12
+#>    LR      group group_n_nodes group_n_edges group_adhesion group_motif_count
+#>    <chr>   <int>         <int>         <dbl>          <dbl>             <int>
+#>  1 App_Dcc     1          1095          1957              0              4947
+#>  2 App_Dcc     2           183           310              0               807
+#>  3 App_Dcc    20             8             9              0                15
+#>  4 App_Dcc     5            52            92              0               217
+#>  5 App_Dcc     4            70           115              0               292
+#>  6 App_Dcc     6            45            59              0               151
+#>  7 App_Dcc     9            27            30              0                78
+#>  8 App_Dcc     3           136           275              0               645
+#>  9 App_Dcc     7            30            48              0               111
+#> 10 App_Dcc    17            12            11              0                26
+#> # ℹ 327 more rows
 #> # ℹ 6 more variables: group_diameter <dbl>, group_un_diameter <dbl>,
 #> #   group_mean_dist <dbl>, group_girth <dbl>, group_circuit_rank <dbl>,
 #> #   group_reciprocity <dbl>
 ```
+
+``` r
+LR_of_interest <- "App_Dcc"
+```
+
+``` r
+plot_spatial_ccc_graph(ccc_graph = ccc_graph_list[[LR_of_interest]],
+                       tissue_img = imgRaster(spe),
+                       node_color = "group_diameter",
+                       node_size = 1,
+                       edge_color = "group_diameter",
+                       which_on_top = "edge")
+#> Warning: The `guide` argument in `scale_*()` cannot be `FALSE`. This was deprecated in
+#> ggplot2 3.3.4.
+#> ℹ Please use "none" instead.
+#> ℹ The deprecated feature was likely used in the spatialCCC package.
+#>   Please report the issue at <https://github.com/dolchan/spatialCCC/issues>.
+#> This warning is displayed once every 8 hours.
+#> Call `lifecycle::last_lifecycle_warnings()` to see where this warning was
+#> generated.
+```
+
+<img src="man/figures/README-unnamed-chunk-14-1.png" width="100%" />
+
+``` r
+plot_spatial_ccc_graph(ccc_graph = ccc_graph_list[[LR_of_interest]],
+                       # tissue_img = imgRaster(spe),
+                       node_color = "group",
+                       node_size = 0.1,
+                       edge_color = "group_diameter",
+                       edge_width = 0.1,
+                       which_on_top = "edge")
+```
+
+<img src="man/figures/README-unnamed-chunk-15-1.png" width="100%" />
+
+``` r
+plot_spatial_ccc_graph(ccc_graph = ccc_graph_list[[LR_of_interest]],
+                       # tissue_img = imgRaster(spe),
+                       graph_layout = "stress",
+                       node_color = "group",
+                       edge_color = "group_diameter",
+                       edge_width = 0.25,
+                       which_on_top = "edge")
+```
+
+<img src="man/figures/README-unnamed-chunk-16-1.png" width="100%" />
